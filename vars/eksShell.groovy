166cbaaf4fabd9b5d61eb034f8870618439dcff7
tejtejas <116590947+tejtejas@users.noreply.github.com>
2023-01-07 20:05:14 +0530

    Update eksShell.groovy

--- vars/eksShell.groovy
+++ vars/eksShell.groovy
@@ -12,36 +12,35 @@ environment {
     		containerName = "${contname}"
 	}
 		
-    agent none
+  agent none
 
-    stages {
-        stage("POLL SCM"){
-		agent{label 'docker'}
-            		steps {
-                	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
-            		}
-        } 
-        
-        stage('BUILD IMAGE') {
-		agent{label 'docker'}
-            		steps {
-                	sh 'docker build -t $registry:$dockerTag .'             
-            		}
+  stages {
+      stage('Git Checkout') {
+        agent { label 'docker' }
+        steps {
+          checkout scmGit(branches: [[name: '$gitBranch']], extensions: [], userRemoteConfigs: [[credentialsId: '$gitCredId', url: '$gitRepo']])
         }
-        
-        stage('PUSH HUB') { 
-		agent{label 'docker'}
-            		steps {
-			            sh 'docker push $registry:$dockerTag'                   	
-                	}    
+      }
+
+      stage('build') {
+        agent { label 'docker' }
+        steps {
+          sh 'docker build -t $registry:$dockerTag .'
         }
-        
-        stage('DEPLOY IMAGE') {
-		agent{label 'eks'}
-		          steps {
-			            sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
-		          }
-	}  
-    }
-}  
+      }
+
+      stage('push to dockerhub') {
+        agent { label 'docker' }
+        steps {
+          sh 'sudo docker push $registry:$dockerTag'
+        }
+      }
+
+      stage('deploy to k8s') {
+        agent { label 'eks' }
+        steps {
+          sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
+        }
+      }
+}
 }
