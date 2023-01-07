a6d777c4d8a9090171ede56bbb3417db2347e42f
tejtejas <116590947+tejtejas@users.noreply.github.com>
2023-01-07 16:42:09 +0530

    Update eksShell.groovy

--- vars/eksShell.groovy
+++ vars/eksShell.groovy
@@ -1,1 +1,46 @@
+def call(String registryCred = 'a', String registryname = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a', String depname = 'a', String contname = 'a') {
 
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+    registry = "${registryname}" 	
+		dockerTag = "${docTag}$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+    deployment = "${depname}"
+    containerName = "${contname}"
+	}
+		
+  agent none
+
+  stages {
+      stage('Git Checkout') {
+        agent { label 'docker' }
+        steps {
+          checkout scmGit(branches: [[name: '$gitBranch']], extensions: [], userRemoteConfigs: [[credentialsId: '$gitCredId', url: '$gitRepo']])
+        }
+      }
+
+      stage('build') {
+        agent { label 'docker' }
+        steps {
+          sh 'docker build -t $registry:$dockerTag .'
+        }
+      }
+
+      stage('push to dockerhub') {
+        agent { label 'docker' }
+        steps {
+          sh 'docker push $registry:$dockerTag'
+        }
+      }
+
+      stage('deploy to k8s') {
+        agent { label 'eks' }
+        steps {
+          sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
+        }
+      }
+}
+}
