31eb8190f58fd8aa3522edeb91b6daf7b3412418
tejtejas <116590947+tejtejas@users.noreply.github.com>
2023-01-07 19:49:54 +0530

    Update eksShell.groovy

--- vars/eksShell.groovy
+++ vars/eksShell.groovy
@@ -3,44 +3,45 @@ def call(String registryCred = 'a', String registryname = 'a', String docTag = '
 pipeline {
 environment { 
 		registryCredential = "${registryCred}"
-    registry = "${registryname}" 	
+    		registry = "${registryname}" 	
 		dockerTag = "${docTag}$BUILD_NUMBER"
 		gitRepo = "${grepo}"
 		gitBranch = "${gbranch}"
 		gitCredId = "${gitcred}"
-    deployment = "${depname}"
-    containerName = "${contname}"
+    		deployment = "${depname}"
+    		containerName = "${contname}"
 	}
 		
-  agent none
+    agent none
 
-  stages {
-      stage('Git Checkout') {
-        agent { label 'docker' }
-        steps {
-          checkout scmGit(branches: [[name: '$gitBranch']], extensions: [], userRemoteConfigs: [[credentialsId: '$gitCredId', url: '$gitRepo']])
+    stages {
+        stage("POLL SCM"){
+		agent{label 'docker'}
+            		steps {
+                	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
+            		}
+        } 
+        
+        stage('BUILD IMAGE') {
+		agent{label 'docker'}
+            		steps {
+                	sh 'docker build -t $registry:$dockerTag .'             
+            		}
         }
-      }
-
-      stage('build') {
-        agent { label 'docker' }
-        steps {
-          sh 'docker build -t $registry:$dockerTag .'
+        
+        stage('PUSH HUB') { 
+		agent{label 'docker'}
+            		steps {
+			            sh 'docker push $registry:$dockerTag'                   	
+                	}    
         }
-      }
-
-      stage('push to dockerhub') {
-        agent { label 'docker' }
-        steps {
-          sh 'docker push $registry:$dockerTag'
-        }
-      }
-
-      stage('deploy to k8s') {
-        agent { label 'eks' }
-        steps {
-          sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
-        }
-      }
-}
+        
+        stage('DEPLOY IMAGE') {
+		agent{label 'eks'}
+		          steps {
+			            sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
+		          }
+	}  
+    }
+}  
 }
